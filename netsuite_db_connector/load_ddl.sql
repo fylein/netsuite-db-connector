2271b6ec3314a48f0839a6b9801df1e1bd0be233
Sivaramakrishnan Narayanan <siva@fyle.in>
2019-11-24 16:34:30 +0530

    Load tables

--- netsuite_db_connector/load_ddl.sql
+++ netsuite_db_connector/load_ddl.sql
@@ -1,1 +1,22 @@
--- TODO: add tables
+drop table if exists ns_load_vendor_bills;
+
+create table ns_load_vendor_bills(
+    "externalId" text primary key,
+    "currencyInternalId" text,
+    "memo" text not null,
+    "classInternalId" text,
+    "locationInternalId" text,
+    "entityInternalId" text not null
+);
+
+drop table if exists ns_load_vendor_bills_expenses;
+
+create table ns_load_vendor_bills_expenses(
+    "vendorBillExternalId" text,
+    "accountInternalId" text not null,
+    "amount" numeric,
+    "departmentInternalId" text,
+    "classInternalId" text,
+    "locationInternalId" text,
+    "memo" text
+);
